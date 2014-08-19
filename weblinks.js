b1183705b32400d00887a468d3d59d950e8f5b12
ctf005 <chris.falch@amfam.com>
2014-08-19 14:17:33 -0500

    Adds a custom directive 'display-weblinks'. Prior Git commit had identical functionality, but used a controller (ng-controller="WebLinksController as linkCtrl") directly from weblinks.html. This controller data is moved to html fragment 'display-weblinks.html'.

--- weblinks.js
+++ weblinks.js
@@ -1,12 +1,18 @@
 (function() {
 	var app = angular.module('weblinks', []);
 
-	app.controller('WebLinksController', [ '$http', function($http){
-		var linksApp = this;
-		linksApp.groups = [];
-		$http.get('urls.json').success(function(data){
-			linksApp.groups = data;
-		});
-	} ]);
+	app.directive("displayWeblinks", function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'display-weblinks.html',
+			controller : ['$http', function($http) {
+				var linksApp = this;
+				linksApp.groups = [];
+				$http.get('urls.json').success(function(data) {
+					linksApp.groups = data;
+				});
+			}], controllerAs:'linkCtrl'
+		};
+	});
 
 })();
