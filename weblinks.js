79cb55f1a4df2f0648a5aa5467f9b787843c5f1e
ctf005 <chris.falch@amfam.com>
2014-08-22 16:07:41 -0500

    First cut at adding controls to update JSON on disk Couldn't find a way to do this natively. Did find a package ZeroClipboard which uses a flash control in the browser. Decided against it.

--- weblinks.js
+++ weblinks.js
@@ -1,16 +1,26 @@
 (function() {
 	var app = angular.module('weblinks', []);
 
+	var jsonFromLocalStorage = false;
+	var jsonFromDisk = false;
+	var jsonData = [];
+
 	app.directive("displayWeblinks", function() {
 		return {
 			restrict: 'E',
 			templateUrl: 'display-weblinks.html',
 			controller : ['$http', function($http) {
 				var linksApp = this;
-				linksApp.groups = [];
-				$http.get('urls.json').success(function(data) {
-					linksApp.groups = data;
-				});
+				linksApp.groups = loadJsonLocal();
+				if(linksApp.groups.length > 0) {
+					jsonFromLocalStorage = true;
+				} else {
+					$http.get('urls.json').success(function(data) {
+						linksApp.groups = data;
+						jsonFromDisk = true;
+					});
+				}
+				jsonData = linksApp.groups;
 				this.editGroup = "";
 				this.isEditing = function(groupName) {
 					return groupName == this.editGroup;
@@ -22,17 +32,87 @@
 				this.addLink = function(group) {
 					group.links.push(this.linkToAdd);
 					$http.post('urls.json', linksApp.groups).success(function(data) {
-						alert('Saved urls.json!');
+						//alert('Saved urls.json!');
 					}).error(function(data) {
-						alert('DID NOT SAVE');
+						//alert('DID NOT SAVE');
 						console.log(data);
 					});
+					saveJsonLocal(linksApp.groups);
+					jsonFromLocalStorage = true;
+					jsonFromDisk = false;
+					jsonData = linksApp.groups; // Update global var; Shouldn't this already be updated by reference?
 					this.linkToAdd = {};
 					this.setEditing('');
 				};
 			}], controllerAs:'linkCtrl'
 		};
 	});
+
+	app.directive("jsonDataControls", function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'json-data-controls.html',
+			controller: function() {
+				this.isJsonFromLocalStorage = function() {
+					return jsonFromLocalStorage;
+				};
+
+				this.isJsonFromDisk = function() {
+					return jsonFromDisk;
+				};
+				this.getWeblinksAsJson = function() {
+					return angular.toJson(jsonData, true);
+				};
+				this.clearJsonLocal = function() {
+					if(typeof(Storage) != "undefined") {
+						window.localStorage.removeItem("weblinks-json");
+						jsonFromLocalStorage = false;
+						jsonFromDisk = true;
+					}
+				};
+			}, controllerAs: 'dataCtrl'
+		};
+	});
+		
+	app.controller("SearchController", function() {
+		this.queryStr = "";
+	});
+
+	/** Can't seem to get this to work, do the asynchronous nature of the callback
+	var getWeblinks = function($http) {
+		var weblinks = [];
+		$http.get('urls.json').success(function(data) {
+			weblinks = data;
+			//alert(angular.toJson(weblinks));
+			return weblinks;
+		});
+	};
+	**/
+
+	var loadJsonLocal = function() {
+		var weblinksJson = [];
+		if(typeof(Storage) != "undefined") {
+			var data = window.localStorage.getItem("weblinks-json");
+			if(data) {
+				weblinksJson = angular.fromJson(data);
+			}
+		}
+		return weblinksJson;
+	};
+
+	var saveJsonLocal = function(jsonToWrite) {
+		if(typeof(Storage) != "undefined") {
+			window.localStorage.setItem("weblinks-json", angular.toJson(jsonToWrite, true)); // pretty-print
+		} else {
+			alert("Could not write to local storage!");
+		}
+	};
+
+	var clearJsonLocal = function() {
+		if(typeof(Storage) != "undefined") {
+			window.localStorage.removeItem("weblinks-json");
+		}
+	};
 
 })();
 
